cac0441b9662e7e66ce9015ccefbc4b850443dcb
MeryL1997 <mery.lema@pucese.edu.ec>
2020-06-30 11:03:46 -0500

    actulizacion de config

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 module.exports = () => {
   return mysql.createConnection({
-    host: 'localhost',
-    user: 'usuario',
-    password: 'contraseña',
-    database: 'nombre de la base de datos',
+    host: '68.183.102.193',
+    user: 'root',
+    password: '2020Mery',
+    database: 'classicmodels',
     port: ''
   });
 }
