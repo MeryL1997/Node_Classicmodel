34e619405bb6102cd0b3cdeb2f6c9e8e32565ffd
MeryL1997 <mery.lema@pucese.edu.ec>
2020-07-03 09:40:26 -0500

    view1

--- src/app/routes/view1.js
+++ src/app/routes/view1.js
@@ -5,7 +5,7 @@ module.exports = app => {
   const connection = dbConnection();
 
   app.get('/view1', (req, res) => {
-    connection.query('SELECT * FROM Cliente_mayor_cantidad_compras', (err, result) => {
+    connection.query('SELECT * FROM classicmodels.Cliente_mayor_cantidad_compras', (err, result) => {
       res.render('views/view1', {
         view1: result
       });
--- src/app/routes/view2.js
+++ src/app/routes/view2.js
@@ -5,7 +5,7 @@ module.exports = app => {
   const connection = dbConnection();
 
   app.get('/view2', (req, res) => {
-    connection.query('SELECT * FROM Productos_Mas_Vendido', (err, result) => {
+    connection.query('SELECT * FROM classicmodels.Productos_Mas_Vendido', (err, result) => {
       res.render('views/view2', {
         view2: result
       });
