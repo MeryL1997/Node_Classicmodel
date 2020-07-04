5eefb2203bd5493e12b487897434098f76cd66ee
MeryL1997 <mery.lema@pucese.edu.ec>
2020-07-04 05:20:19 -0500

    actulzacion de rutas

--- src/app/routes/formulario.js
+++ src/app/routes/formulario.js
@@ -4,7 +4,7 @@ const dbConnection = require("../../config/dbConnection");
 module.exports = app => {
 
     const connection = dbConnection();
-    app.post('/ordenar', (req, res) => {
+    app.post('/formulario', (req, res) => {
     // Order
     var a1=req.body.new_orderNumber;
     var a2=req.body.new_orderDate;
