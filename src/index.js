93e806444e1f9f25bd8027e75393702cfba98928
MeryL1997 <mery.lema@pucese.edu.ec>
2020-07-04 07:13:51 -0500

    modificando la vista 4 con formulario

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require("./config/server");
 
+require("./app/routes/view4")(app);
 require("./app/routes/view3")(app);
 require("./app/routes/view2")(app);
 require("./app/routes/view1")(app);
