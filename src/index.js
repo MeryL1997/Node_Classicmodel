cb2dedd07a6963e7d9cbd0b25ccb26a57252d457
MeryL1997 <mery.lema@pucese.edu.ec>
2020-07-04 06:17:58 -0500

    modificando la vista

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const app = require("./config/server");
 
+require("./app/routes/view3")(app);
 require("./app/routes/view2")(app);
 require("./app/routes/view1")(app);
 require("./app/routes/home")(app);
-require("./app/routes/ordenes")(app);
 
 // starting the server
 app.listen(app.get("port"), () => {
