245022db9912b5577c78167946a8349338f61036
MeryL1997 <mery.lema@pucese.edu.ec>
2020-07-04 07:40:53 -0500

    soluciando el erro4

--- src/app/routes/view4.js
+++ src/app/routes/view4.js
@@ -31,7 +31,7 @@ module.exports = app => {
      && a10!== undefined
      && a11!== undefined
      ){
-     connection.query(`call classicmodels.Nuevo_Pedido('${a1}','${a2}','${a3}','${a4}','${a5}','${a6}','${a7}','${a8}','${a9}','${a10}','${a11}')`,
+     connection.query(`call Nuevo_Pedido('${a1}','${a2}','${a3}','${a4}','${a5}','${a6}','${a7}','${a8}','${a9}','${a10}','${a11}')`,
      function(err,rows) {
        if(err) {
          console.log(err);
