4b979d7034238ac27759c97bb801bae5fc1e319f
MeryL1997 <mery.lema@pucese.edu.ec>
2020-07-04 05:58:37 -0500

    elimine formulario

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const app = require("./config/server");
 require("./app/routes/view2")(app);
 require("./app/routes/view1")(app);
 require("./app/routes/home")(app);
-require("./app/routes/formulario")(app);
 require("./app/routes/ordenes")(app);
 
 // starting the server
